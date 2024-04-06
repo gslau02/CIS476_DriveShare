e81899293f1e940ec3b056f488de6718b7afa40e
Aidil Najmir <102715539+aidilnajmir@users.noreply.github.com>
2024-04-06 03:53:05 -0400

    update booking utils

--- client/src/utils/booking.js
+++ client/src/utils/booking.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const API_ENDPOINT = 'http://localhost:3001/booking';
+const API_ENDPOINT = 'http://localhost:3001/bookings'
 
-export const fetchBookingsByUser = async (userId) => {
+export const fetchUserBookings = async (userId) => {
   try {
-    const response = await axios.get(`${API_ENDPOINT}/${userId}`);
+    const response = await axios.get(`${API_ENDPOINT}/user/${userId}`);
     return response.data;
   } catch (error) {
-    console.error(error);
-    throw error;
+    console.error('Error fetching user bookings:', error);
+    throw new Error('Failed to fetch user bookings. Please try again.');
   }
 };
