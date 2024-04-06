2e0c0f7f8de2744ccd3c0d61254b50b4d97cf289
gslau02 <114629753+gslau02@users.noreply.github.com>
2024-04-05 22:50:01 -0400

    Added logout

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { register, auth, verifySession } = require('../controllers/authController');
+const { register, auth, verifySession, logout } = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/auth', auth);
 router.post('/verify-session', verifySession);
+router.post('/logout', logout);
 
 module.exports = router;
