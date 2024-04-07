ec320be183e89d8d13c49cc47f8f5b7ee0cb17b5
Aidil Najmir <102715539+aidilnajmir@users.noreply.github.com>
2024-04-06 22:35:48 -0400

    Add files via upload

--- server/routes/inbox.js
+++ server/routes/inbox.js
@@ -1,33 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const Message = require('../models/Message');
-const Notification = require('../models/Notification');
+const { fetchMessages, fetchNotifications } = require('../controllers/inboxController');
 
-// GET endpoint to retrieve all messages for a user
-router.get('/messages/:userId', async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const messages = await Message.find({
-      // Assuming you have defined sender and receiver accordingly in your Message model
-      $or: [{ sender: userId }, { receiver: userId }],
-    }).sort({ createdAt: -1 }); // Sorting by newest first, as an example
-
-    res.json(messages);
-  } catch (error) {
-    res.status(500).json({ message: 'Error fetching messages', error });
-  }
-});
-
-// GET endpoint to retrieve all notifications for a user
-router.get('/notifications/:userId', async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const notifications = await Notification.find({ user: userId }).sort({ createdAt: -1 });
-
-    res.json(notifications);
-  } catch (error) {
-    res.status(500).json({ message: 'Error fetching notifications', error });
-  }
-});
+router.get('/messages', fetchMessages);
+router.get('/notifications', fetchNotifications);
 
 module.exports = router;
