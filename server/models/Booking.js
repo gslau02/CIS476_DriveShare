950177e4e083159127e1f2edbcab572dc99707a6
gslau02 <114629753+gslau02@users.noreply.github.com>
2024-04-07 17:12:22 -0400

    Removed "REVIEWED" status

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -65,7 +65,7 @@ const bookingSchema = new mongoose.Schema({
   carId: { type: mongoose.Schema.Types.ObjectId, ref: 'Car', required: true },
   startDate: { type: Date, required: true },
   endDate: { type: Date, required: true },
-  status: { type: String, enum: ['REQUESTED', 'ACTIVE', 'COMPLETED', 'REVIEWED'], default: 'REQUESTED' },
+  status: { type: String, enum: ['REQUESTED', 'ACTIVE', 'COMPLETED'], default: 'REQUESTED' },
   renterReview: {
     rating: { type: Number, min: 0, max: 5, default: null },
     feedback: { type: String, default: '' }
