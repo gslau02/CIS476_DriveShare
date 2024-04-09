21a6edb8bdf1eb47b926d8776a0a0d19dd7075da
Muhammad Adib Bin Halizam <adibhalizam@gmail.com>
2024-04-09 01:15:36 -0400

    Add comments to authController.js bookingController.js, carController.js

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -4,16 +4,22 @@ const User = require('../models/User');
 const Notification = require('../models/Notification');
 const Car = require('../models/Car');
 const NotificationObserver = require('../utils/observer/NotificationObserver');
-// Create an instance of NotificationObserver
 const Subject = require('../utils/observer/Subject');
+
+// Create an instance of BookingSubject and NotificationObserver
 const BookingSubject = new Subject();
 const notificationObserver = new NotificationObserver(BookingSubject);
 
+// Function to create a new booking
 const createBooking = async (req, res) => {
   try {
+    // Extract data from request body
     const { renterId, carId, startDate, endDate, ownerId } = req.body;
+    // Create a new Booking instance
     const booking = new Booking({ renterId, carId, startDate, endDate, status: 'REQUESTED' });
+    // Save the booking to the database
     await booking.save();
+    // Update booking status
     await updateBookingStatus();
     
     // Extract the ID of the newly created booking
@@ -27,6 +33,7 @@ const createBooking = async (req, res) => {
     const renterMessage = `Your booking for ${carName} was successful`;
     const ownerMessage = `Your car (${carName}) has been booked`;
 
+    // Notify users about the booking
     BookingSubject.notifyObservers(renterId, renterMessage);
     BookingSubject.notifyObservers(ownerId, ownerMessage);
 
@@ -38,11 +45,14 @@ const createBooking = async (req, res) => {
   }
 };
 
+// Function to check for booking clashes
 const checkForClashes = async (req, res) => {
   try {
+    // Extract parameters from request
     const { carId } = req.params;
     const { startDate, endDate } = req.query;
 
+    // Find bookings that clash with the requested dates
     const clashes = await Booking.find({
       carId,
       $or: [
@@ -51,6 +61,7 @@ const checkForClashes = async (req, res) => {
       ]
     });
 
+    // Return clashes in the response
     res.status(200).json({ clashes });
   } catch (error) {
     console.error('Error checking for clashes:', error);
@@ -58,9 +69,11 @@ const checkForClashes = async (req, res) => {
   }
 };
 
+// Function to update booking status
 const updateBookingStatus = async () => {
   try {
     const now = new Date();
+    // Update status of bookings based on current date
     await Booking.updateMany(
       { endDate: { $lt: now }, status: { $ne: 'COMPLETED' } },
       { $set: { status: 'COMPLETED' } }
@@ -77,9 +90,12 @@ const updateBookingStatus = async () => {
 // Schedule the updateBookingStatus function to run every minute
 cron.schedule('* * * * *', updateBookingStatus);
 
+// Function to activate a booking
 const activateBooking = async (req, res) => {
   try {
+    // Extract booking ID from request parameters
     const { bookingId } = req.params;
+    // Update booking status to ACTIVE
     await Booking.findByIdAndUpdate(bookingId, { status: 'ACTIVE' });
     res.status(200).json({ message: 'Booking activated successfully' });
   } catch (error) {
@@ -88,11 +104,15 @@ const activateBooking = async (req, res) => {
   }
 };
 
+// Function to fetch bookings by user
 const fetchBookingsByUser = async (req, res) => {
   try {
+    // Extract user ID from request parameters
     const { userId } = req.params;
+    // Find all bookings related to the user
     const allBookings = await Booking.find({ renterId: userId });
 
+    // Filter active and past bookings
     const now = new Date();
     const activeBookings = await Promise.all(
       allBookings
@@ -113,7 +133,8 @@ const fetchBookingsByUser = async (req, res) => {
           return { ...booking.toObject(), car, owner };
         })
     );
-
+    
+    // Return active and past bookings
     res.json({ activeBookings, pastBookings });
   } catch (error) {
     console.error('Error retrieving bookings:', error);
@@ -121,16 +142,22 @@ const fetchBookingsByUser = async (req, res) => {
   }
 };
 
+// Function to fetch orders by owner
 const fetchOrdersByOwner = async (req, res) => {
   try {
+    // Extract user ID from request parameters
     const { userId } = req.params;
+    // Find owner details
     const owner = await User.findById(userId);
+    // Find cars owned by the user
     const cars = await Car.find({ owner: owner._id }).select('make model year pickUpLocation owner');
     const carIds = cars.map(car => car._id); // Extract unique carIds
 
+    // Find orders related to the cars
     const allOrders = await Booking.find({ carId: { $in: carIds } });
     
     const now = new Date();
+    // Filter active and past orders
     const activeOrders = await Promise.all(
       allOrders
         .filter(order => order.endDate > now)
@@ -150,7 +177,8 @@ const fetchOrdersByOwner = async (req, res) => {
           return { ...order.toObject(), car, renter };
         })
     );
-
+    
+    // Return active and past orders
     res.json({ activeOrders, pastOrders });
   } catch (error) {
     console.error('Error retrieving orders:', error);
@@ -158,9 +186,12 @@ const fetchOrdersByOwner = async (req, res) => {
   }
 };
 
+// Function to post renter's review
 const postRenterReview = async (req, res) => {
   try {
+    // Extract booking ID from request parameters
     const { bookingId } = req.params;
+    // Extract rating and feedback from request body
     const { rating, feedback } = req.body;
 
     // Find the booking by ID
@@ -182,11 +213,8 @@ const postRenterReview = async (req, res) => {
     const carName = car ? `${car.make} ${car.model}` : 'Unknown Car'; // Get the car name
 
     // Notify the owner about the renter's review
-    // const ownerMessage = `The renter has submitted a review for the booking ${bookingId}`;
     const ownerMessage = `The renter has submitted a review for the ${carName} booking`;
-
     BookingSubject.notifyObservers(car.owner, ownerMessage);
-    // notificationObserver.update(car.owner, ownerMessage);
     
     res.status(200).json({ message: 'Review submitted successfully' });
   } catch (error) {
@@ -195,9 +223,12 @@ const postRenterReview = async (req, res) => {
   }
 };
 
+// Function to post owner's review
 const postOnwerReview = async (req, res) => {
   try {
+    // Extract order ID from request parameters
     const { orderId } = req.params;
+    // Extract rating and feedback from request body
     const { rating, feedback } = req.body;
 
     // Find the booking by ID
@@ -219,11 +250,8 @@ const postOnwerReview = async (req, res) => {
     const carName = car ? `${car.make} ${car.model}` : 'Unknown Car'; // Get the car name
 
     // Notify the renter about the owner's review
-    // const renterMessage = `The owner has submitted a review for the booking ${orderId}`;
     const renterMessage = `The owner has submitted a review for the ${carName} booking`;
-
     BookingSubject.notifyObservers(order.renterId, renterMessage);
-    // notificationObserver.update(order.renterId, renterMessage);
 
     res.status(200).json({ message: 'Review submitted successfully' });
   } catch (error) {
