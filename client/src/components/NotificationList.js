047662758f664fb5b7371a5585cb6172df58ee97
Aidil Najmir <aidilnjmr@gmail.com>
2024-04-08 23:19:01 -0400

    update component comments

--- client/src/components/NotificationList.js
+++ client/src/components/NotificationList.js
@@ -3,13 +3,17 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import NotificationCard from './NotificationCard';
 
+// NotificationList component that takes userId
 const NotificationList = ({ userId }) => {
   const [notifications, setNotifications] = useState([]);
 
+  // Fetch notification when userId changes using Effect hook
   useEffect(() => {
+    // Fetch notifications based on userId
     const fetchNotifications = async () => {
       try {
         const response = await axios.get(`http://localhost:3001/notification/getNotificationsByUser/${userId}`);
+        // Update notifications state
         setNotifications(response.data.notifications);
       } catch (error) {
         console.error('Error fetching notifications:', error);
@@ -18,6 +22,7 @@ const NotificationList = ({ userId }) => {
     fetchNotifications();
   }, [userId]);
 
+  // JSX structure to render NotificationList
   return (
     <div>
       {notifications.length === 0 ? (
