eec7ecd0b44009b28b320a33dde463cd0db8658d
gslau02 <114629753+gslau02@users.noreply.github.com>
2024-04-09 00:11:57 -0400

    Commented NotificationList.js

--- client/src/components/NotificationList/NotificationList.js
+++ client/src/components/NotificationList/NotificationList.js
@@ -2,14 +2,16 @@
 import React from 'react';
 import NotificationCard from '../NotificationCard';
 
+// NotificationList component that takes notifications as an object
 const NotificationList = ({ notifications }) => {
+  // JSX structure to render NotificationCard
   return (
     <div>
       {notifications.length === 0 ? (
-        <p>No notifications found.</p>
+        <p>No notifications found.</p> // If no notifications are found, display this message
       ) : (
         notifications.map(notification => (
-          <NotificationCard key={notification._id} title={notification.message} date={notification.timestamp} />
+          <NotificationCard key={notification._id} title={notification.message} date={notification.timestamp} /> // Display notification card with title and date
         ))
       )}
     </div>
