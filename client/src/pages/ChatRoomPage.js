1eb6f9d67f8b192b2e908cd999411feba2a74528
Muhammad Adib Bin Halizam <adibhalizam@gmail.com>
2024-04-08 22:48:41 -0400

    Add comment ChatRoomPage.js

--- client/src/pages/ChatRoomPage.js
+++ client/src/pages/ChatRoomPage.js
@@ -4,18 +4,24 @@ import { createMessage, fetchChatRoomMessages } from '../utils/inbox';
 import MessageDisplay from '../components/MessageDisplay';
 
 const ChatRoomPage = () => {
+  // Get recipient from location state
   const location = useLocation();
   const { recipient } = location.state;
+
+  // Get user ID from localStorage
   const userId = localStorage.getItem('userId');
 
+  // Define state for input message and loaded messages
   const [inputMessage, setInputMessage] = useState('');
   const [loadedMessages, setLoadedMessages] = useState([]);
 
+  // Fetch messages on component mount
   useEffect(() => {
     const fetchMessages = async () => {
       try {
         const messages = await fetchChatRoomMessages(userId, recipient._id);
         console.log(messages);
+        // Set loaded messages state
         setLoadedMessages(messages);
       } catch (error) {
         console.error('Failed to fetch messages:', error);
@@ -25,18 +31,22 @@ const ChatRoomPage = () => {
     fetchMessages();
   }, [userId, recipient._id]);
 
+  // Define message data object
   const messageData = { 
     sender: userId,
     recipient: recipient._id, 
     content: inputMessage
   }
 
+  // Handle input message change
   const handleInputMessageChange = (e) => {
     setInputMessage(e.target.value);
   };
 
+  // Handle sending message
   const handleSendMessage = () => {
     if (inputMessage.trim() !== '') {
+      // Create message and update loaded messages state
       createMessage(messageData);
       const newMessage = { sender: userId, recipient: recipient._id, content: inputMessage, createdAt: new Date() };
       setLoadedMessages([...loadedMessages, newMessage]);
@@ -44,6 +54,7 @@ const ChatRoomPage = () => {
     }
   };  
 
+  // Render the component
   return (
     <div style={{ textAlign:'-webkit-center' }}>
       <h2>{recipient.name}</h2>
